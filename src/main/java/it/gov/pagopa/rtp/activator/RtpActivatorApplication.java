338c1ad052930774661e1b445d05c143865fe61d
Luca Consalvi <luca.consalvi@pagopa.it>
2024-11-26 15:13:41 +0100

    add test for controller

--- src/main/java/it/gov/pagopa/rtp/activator/RtpActivatorApplication.java
+++ src/main/java/it/gov/pagopa/rtp/activator/RtpActivatorApplication.java
@@ -3,9 +3,13 @@ package it.gov.pagopa.rtp.activator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+import com.azure.spring.data.cosmos.repository.config.EnableReactiveCosmosRepositories;
+
 import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
+@EnableReactiveCosmosRepositories
 @ConfigurationPropertiesScan()
 public class RtpActivatorApplication {
 
