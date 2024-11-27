1fc83a25141d56dfe9d670e174dcefde4d3dbe5f
Luca Consalvi <luca.consalvi@pagopa.it>
2024-11-27 16:16:43 +0100

    try to fix issue

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/CosmosDBConfig.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/CosmosDBConfig.java
@@ -1,10 +1,18 @@
 package it.gov.pagopa.rtp.activator.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.azure.spring.cloud.autoconfigure.implementation.context.properties.AzureGlobalProperties;
 import com.azure.spring.data.cosmos.repository.config.EnableReactiveCosmosRepositories;
 
 @Configuration
 @EnableReactiveCosmosRepositories("it.gov.pagopa.rtp.activator.repository")
 public class CosmosDBConfig {
+
+    @Bean
+    public AzureGlobalProperties azureGlobalProperties() {
+        return new AzureGlobalProperties();
+    }
+
 }
