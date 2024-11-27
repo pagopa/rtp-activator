aeb104a78f9144f344dbb116f5ced96ff5920c05
Luca Consalvi <luca.consalvi@pagopa.it>
2024-11-27 11:40:24 +0100

    update tests

---
+++ src/test/java/it/gov/pagopa/rtp/activator/configuration/CosmosPropertiesConfigTest.java
@@ -0,0 +1,28 @@
+package it.gov.pagopa.rtp.activator.configuration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@EnableConfigurationProperties(value = CosmosPropertiesConfig.class)
+@TestPropertySource("classpath:application.properties")
+public class CosmosPropertiesConfigTest {
+
+    @Autowired
+    private CosmosPropertiesConfig cosmosPropertiesConfig;
+
+    @Test
+    void testPropertiesLoaded() {
+        assertNotNull(cosmosPropertiesConfig);
+        assertEquals("https://example.com/db/endpoint", cosmosPropertiesConfig.getEndpoint());
+        assertEquals("rtp", cosmosPropertiesConfig.getDbName());
+
+    }
+}
