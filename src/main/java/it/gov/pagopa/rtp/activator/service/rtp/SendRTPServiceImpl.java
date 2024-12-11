71412f8aed18ff5a2af44bae0fdadeb404f397a9
Luca Consalvi <luca.consalvi@pagopa.it>
2024-12-11 12:01:16 +0100

    fix log object mapper

--- src/main/java/it/gov/pagopa/rtp/activator/service/rtp/SendRTPServiceImpl.java
+++ src/main/java/it/gov/pagopa/rtp/activator/service/rtp/SendRTPServiceImpl.java
@@ -2,12 +2,14 @@ package it.gov.pagopa.rtp.activator.service.rtp;
 
 import it.gov.pagopa.rtp.activator.domain.rtp.ResourceID;
 import it.gov.pagopa.rtp.activator.domain.rtp.Rtp;
+import it.gov.pagopa.rtp.activator.model.generated.epc.SepaRequestToPayRequestResourceDto;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -16,6 +18,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Service
 @Slf4j
+@RegisterReflectionForBinding({ SepaRequestToPayRequestResourceDto.class })
 public class SendRTPServiceImpl implements SendRTPService {
 
   private SepaRequestToPayMapper sepaRequestToPayMapper;
@@ -25,7 +28,8 @@ public class SendRTPServiceImpl implements SendRTPService {
   }
 
   @Override
-  public Mono<Rtp> send(String noticeNumber, Integer amount, String description, LocalDate expiryDate, String payerId ,String payeeName,
+  public Mono<Rtp> send(String noticeNumber, Integer amount, String description, LocalDate expiryDate, String payerId,
+      String payeeName,
       String payeeId, String rtpSpId, String endToEndId, String iban, String payTrxRef, String flgConf) {
 
     Rtp rtp = new Rtp(noticeNumber, amount, description, expiryDate, payerId, payeeName, payeeId,
