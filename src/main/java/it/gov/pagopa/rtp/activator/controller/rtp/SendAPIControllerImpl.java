d14967e5c322dc0a5d264e21237196b344f12e1b
Andrea Morabito <andrea.morabito@pagopa.it>
2025-01-02 13:16:56 +0100

    handle error status in controller

--- src/main/java/it/gov/pagopa/rtp/activator/controller/rtp/SendAPIControllerImpl.java
+++ src/main/java/it/gov/pagopa/rtp/activator/controller/rtp/SendAPIControllerImpl.java
@@ -4,10 +4,12 @@ import it.gov.pagopa.rtp.activator.controller.generated.send.RtpsApi;
 import it.gov.pagopa.rtp.activator.model.generated.send.CreateRtpDto;
 import it.gov.pagopa.rtp.activator.service.rtp.SendRTPService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -29,8 +31,15 @@ public class SendAPIControllerImpl implements RtpsApi {
     public Mono<ResponseEntity<Void>> createRtp(Mono<CreateRtpDto> createRtpDto,
             ServerWebExchange exchange) {
         return createRtpDto
-                .map(rtpMapper::toRtp)
-                .flatMap(sendRTPService::send)
-                .thenReturn(ResponseEntity.status(201).build());
+            .map(rtpMapper::toRtp)
+            .flatMap(sendRTPService::send)
+            .thenReturn(new ResponseEntity<Void>(HttpStatus.CREATED))
+            .onErrorResume(e -> {
+                if (e instanceof WebClientResponseException webClientResponseException) {
+                    return Mono.just(
+                        ResponseEntity.status(webClientResponseException.getStatusCode()).build());
+                }
+                return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+            });
     }
 }
