47ca08f9e05602d4d6c93bc9be57e8e72bb594f0
Andrea Morabito <andrea.morabito@pagopa.it>
2025-01-03 16:31:37 +0100

    use custom business exception to define response http status code

--- src/main/java/it/gov/pagopa/rtp/activator/controller/rtp/SendAPIControllerImpl.java
+++ src/main/java/it/gov/pagopa/rtp/activator/controller/rtp/SendAPIControllerImpl.java
@@ -1,16 +1,15 @@
 package it.gov.pagopa.rtp.activator.controller.rtp;
 
 import it.gov.pagopa.rtp.activator.controller.generated.send.RtpsApi;
+import it.gov.pagopa.rtp.activator.domain.errors.PayerNotFoundException;
 import it.gov.pagopa.rtp.activator.model.generated.send.CreateRtpDto;
 import it.gov.pagopa.rtp.activator.service.rtp.SendRTPService;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.support.WebExchangeBindException;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -36,14 +35,13 @@ public class SendAPIControllerImpl implements RtpsApi {
             .flatMap(sendRTPService::send)
             .thenReturn(new ResponseEntity<Void>(HttpStatus.CREATED))
             .onErrorResume(e -> {
-                if (e instanceof WebClientResponseException webClientResponseException) {
-                    return Mono.just(
-                        ResponseEntity.status(webClientResponseException.getStatusCode()).build());
+                if (e instanceof PayerNotFoundException) {
+                    return Mono.just(ResponseEntity.unprocessableEntity().build());
                 }
                 if (e instanceof WebExchangeBindException) {
-                    return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+                    return Mono.just(ResponseEntity.badRequest().build());
                 }
-                return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+                return Mono.just(ResponseEntity.internalServerError().build());
             });
     }
 }
