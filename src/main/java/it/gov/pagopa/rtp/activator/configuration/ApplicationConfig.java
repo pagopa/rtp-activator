126796c5bdfb4ea06b5ff56b7c7ae0376b0b151e
Andrea Morabito <andrea.morabito@pagopa.it>
2025-01-03 16:30:32 +0100

    add bearer token propagation with webclient

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/ApplicationConfig.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/ApplicationConfig.java
@@ -1,15 +1,26 @@
 package it.gov.pagopa.rtp.activator.configuration;
 
 import it.gov.pagopa.rtp.activator.activateClient.api.ReadApi;
+import it.gov.pagopa.rtp.activator.activateClient.invoker.ApiClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ApplicationConfig {
 
   @Bean
-  public ReadApi readApi() {
-    return new ReadApi();
+  public WebClient webClient() {
+    return WebClientInterceptor.createWebClient(WebClient.builder());
   }
 
+  @Bean
+  public ApiClient apiClient(WebClient webClient) {
+    return new ApiClient(webClient);
+  }
+
+  @Bean
+  public ReadApi readApi(ApiClient apiClient) {
+    return new ReadApi(apiClient);
+  }
 }
