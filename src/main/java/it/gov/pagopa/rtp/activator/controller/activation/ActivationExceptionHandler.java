2da5260a354d572eff486e6d47181f007eaf0c39
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2025-01-13 14:31:40 +0100

    fix: [SRTP-201] return 404 on find activation when user is not found (#46)

---
+++ src/main/java/it/gov/pagopa/rtp/activator/controller/activation/ActivationExceptionHandler.java
@@ -0,0 +1,27 @@
+package it.gov.pagopa.rtp.activator.controller.activation;
+
+import it.gov.pagopa.rtp.activator.model.generated.activate.ErrorDto;
+import it.gov.pagopa.rtp.activator.model.generated.activate.ErrorsDto;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = "it.gov.pagopa.rtp.activator.controller.activation")
+public class ActivationExceptionHandler {
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ErrorsDto> handleConstraintViolation(ConstraintViolationException ex) {
+    var errors = ex.getConstraintViolations().stream()
+        .map(cv -> new ErrorDto()
+            .code(cv.getMessageTemplate())
+            .description(cv.getInvalidValue() + " " + cv.getMessage()))
+        .toList();
+    ErrorsDto errorsDto = new ErrorsDto();
+    errorsDto.setErrors(errors);
+    return ResponseEntity.badRequest().body(errorsDto);
+  }
+}
