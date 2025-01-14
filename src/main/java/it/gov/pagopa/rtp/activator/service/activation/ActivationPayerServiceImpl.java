d3b94e7c5cf43b6828ab4316a5d2d75fe50e5e9b
Andrea Morabito <andrea.morabito@pagopa.it>
2025-01-14 11:23:42 +0100

    indent

--- src/main/java/it/gov/pagopa/rtp/activator/service/activation/ActivationPayerServiceImpl.java
+++ src/main/java/it/gov/pagopa/rtp/activator/service/activation/ActivationPayerServiceImpl.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.rtp.activator.service.activation;
 
 import java.time.Instant;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import it.gov.pagopa.rtp.activator.domain.errors.PayerAlreadyExists;
@@ -11,6 +12,7 @@ import it.gov.pagopa.rtp.activator.repository.activation.ActivationDBRepository;
 import reactor.core.publisher.Mono;
 
 @Service
+@Slf4j
 public class ActivationPayerServiceImpl implements ActivationPayerService {
 
     private final ActivationDBRepository activationDBRepository;
@@ -24,7 +26,7 @@ public class ActivationPayerServiceImpl implements ActivationPayerService {
 
         ActivationID activationID = ActivationID.createNew();
         Payer payer = new Payer(activationID, rtpSpId, fiscalCode, Instant.now());
-
+        log.info("Activating payer with fiscal code {}", fiscalCode);
         return activationDBRepository.findByFiscalCode(fiscalCode)
             .flatMap(existingEntity -> Mono.<Payer>error(new PayerAlreadyExists())) 
             .switchIfEmpty(Mono.defer(() -> activationDBRepository.save(payer)));
