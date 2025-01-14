a85c0335b1624300b957001b7b18f63c42837b48
Andrea Morabito <andrea.morabito@pagopa.it>
2025-01-14 11:21:29 +0100

    indent

--- src/main/java/it/gov/pagopa/rtp/activator/domain/rtp/ResourceID.java
+++ src/main/java/it/gov/pagopa/rtp/activator/domain/rtp/ResourceID.java
@@ -7,15 +7,15 @@ import lombok.Getter;
 @Getter
 public class ResourceID {
 
-    private final UUID id;
+  private final UUID id;
 
-    public ResourceID(UUID uuid) {
-        this.id = uuid;
-    }
+  public ResourceID(UUID uuid) {
+    this.id = uuid;
+  }
 
-    public static ResourceID createNew() {
-        UUID uuid = UUID.randomUUID();
-        return new ResourceID(uuid);
-    }
+  public static ResourceID createNew() {
+    UUID uuid = UUID.randomUUID();
+    return new ResourceID(uuid);
+  }
 
 }
