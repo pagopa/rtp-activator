32e4bfaf3c6d2ebf8d38f479e319f45a69c670aa
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2025-01-20 17:39:55 +0100

    fix: [SRTP-204] correct the bad request payload in send rtps (#56)

--- src/main/java/it/gov/pagopa/rtp/activator/controller/rtp/RtpExceptionHandler.java
+++ src/main/java/it/gov/pagopa/rtp/activator/controller/rtp/RtpExceptionHandler.java
@@ -7,17 +7,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @RestControllerAdvice(basePackages = "it.gov.pagopa.rtp.activator.controller.rtp")
 public class RtpExceptionHandler {
 
   @ExceptionHandler(MessageBadFormed.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ResponseEntity<MalformedRequestErrorResponseDto> handleBadRequestFromActivation(MessageBadFormed ex) {
+  public ResponseEntity<MalformedRequestErrorResponseDto> handleBadRequestFromActivation(
+      MessageBadFormed ex) {
     var error = ex.getErrorsDto().getErrors().getFirst();
     var responseBody = new MalformedRequestErrorResponseDto()
         .error(error.getCode())
         .details(error.getDescription());
     return ResponseEntity.badRequest().body(responseBody);
+  }
+
+  @ExceptionHandler(WebExchangeBindException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<MalformedRequestErrorResponseDto> handleConstraintViolation(
+      WebExchangeBindException ex) {
+    return ex.getBindingResult().getFieldErrors().stream()
+        .map(error -> new MalformedRequestErrorResponseDto()
+            .error(error.getCode())
+            .details(error.getRejectedValue() + " " + error.getDefaultMessage()))
+        .findFirst()
+        .map(error -> ResponseEntity.badRequest().body(error))
+        .orElse(ResponseEntity.badRequest().build());
   }
 }
