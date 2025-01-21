f8e8ca3c9f8df2a283e99843e0050509df4e4f51
Andrea Morabito <andrea.morabito@pagopa.it>
2025-01-21 12:33:57 +0100

    add defaultApi bean to send rtp

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/ApplicationConfig.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/ApplicationConfig.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.rtp.activator.configuration;
 
 import it.gov.pagopa.rtp.activator.activateClient.api.ReadApi;
 import it.gov.pagopa.rtp.activator.activateClient.invoker.ApiClient;
+import it.gov.pagopa.rtp.activator.epcClient.api.DefaultApi;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,13 +16,18 @@ public class ApplicationConfig {
     return WebClientInterceptor.createWebClient(WebClient.builder());
   }
 
-  @Bean
+  @Bean("apiClientActivation")
   public ApiClient apiClient(WebClient webClient) {
     return new ApiClient(webClient);
   }
 
   @Bean
-  public ReadApi readApi(ApiClient apiClient) {
+  public ReadApi readApi(@Qualifier("apiClientActivation") ApiClient apiClient) {
     return new ReadApi(apiClient);
+  }
+
+  @Bean
+  public DefaultApi defaultApi() {
+    return new DefaultApi();
   }
 }
