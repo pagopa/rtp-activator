9d297ba7edb519d1d0e1db0d50baeb1e3d066909
Luca Consalvi <luca.consalvi@pagopa.it>
2025-01-23 17:32:57 +0100

    remove withspan annotation

--- src/main/java/it/gov/pagopa/rtp/activator/domain/payer/PayerRepository.java
+++ src/main/java/it/gov/pagopa/rtp/activator/domain/payer/PayerRepository.java
@@ -1,16 +1,13 @@
 package it.gov.pagopa.rtp.activator.domain.payer;
 
 
-import io.opentelemetry.instrumentation.annotations.WithSpan;
 import reactor.core.publisher.Mono;
 
 public interface PayerRepository {
     
     // Used to check if a specific payer is already registered.
-    @WithSpan
     Mono<Payer> findByFiscalCode(String fiscalCode);
     
-    @WithSpan
     Mono<Payer> save(Payer payer);
     
 }
--- src/main/java/it/gov/pagopa/rtp/activator/repository/activation/ActivationDBRepository.java
+++ src/main/java/it/gov/pagopa/rtp/activator/repository/activation/ActivationDBRepository.java
@@ -3,7 +3,6 @@ package it.gov.pagopa.rtp.activator.repository.activation;
 
 import org.springframework.stereotype.Repository;
 
-import io.opentelemetry.instrumentation.annotations.WithSpan;
 import it.gov.pagopa.rtp.activator.domain.payer.Payer;
 import it.gov.pagopa.rtp.activator.domain.payer.PayerRepository;
 import reactor.core.publisher.Mono;
@@ -21,14 +20,12 @@ public class ActivationDBRepository implements PayerRepository {
     }
 
     @Override
-    @WithSpan
     public Mono<Payer> findByFiscalCode(String fiscalCode) {
         return activationDB.findByFiscalCode(fiscalCode)
                 .map(activationMapper::toDomain);
     }
 
     @Override
-    @WithSpan
     public Mono<Payer> save(Payer payer) {
         return activationDB.save(activationMapper.toDbEntity(payer)).map(activationMapper::toDomain);
     }
