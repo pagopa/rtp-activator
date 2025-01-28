4b1dd74876d6f8d8af3eebaa072cf0f49948884b
Luca Consalvi <luca.consalvi@pagopa.it>
2025-01-28 17:08:59 +0100

    add a custom annotation to trace mongo db reactive event and correlate it to the relative http action

--- src/main/java/it/gov/pagopa/rtp/activator/repository/activation/ActivationDB.java
+++ src/main/java/it/gov/pagopa/rtp/activator/repository/activation/ActivationDB.java
@@ -5,9 +5,11 @@ package it.gov.pagopa.rtp.activator.repository.activation;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
+import it.gov.pagopa.rtp.activator.telemetry.TraceMongo;
 import reactor.core.publisher.Mono;
 
 @Repository
+@TraceMongo
 public interface ActivationDB extends ReactiveMongoRepository<ActivationEntity, String> {
     Mono<ActivationEntity> findByFiscalCode(String fiscalCode);
 }
