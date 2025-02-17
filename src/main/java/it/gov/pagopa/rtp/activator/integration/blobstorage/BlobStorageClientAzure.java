a37a3a4ce9bfc99c8a1e9e8c8beae5fd9e6acfb9
Luca Consalvi <luca.consalvi@pagopa.it>
2025-02-17 15:53:50 +0100

    fix tests

--- src/main/java/it/gov/pagopa/rtp/activator/integration/blobstorage/BlobStorageClientAzure.java
+++ src/main/java/it/gov/pagopa/rtp/activator/integration/blobstorage/BlobStorageClientAzure.java
@@ -1,13 +1,11 @@
 package it.gov.pagopa.rtp.activator.integration.blobstorage;
 
 import org.springframework.stereotype.Component;
-
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
-
 import it.gov.pagopa.rtp.activator.configuration.BlobStorageConfig;
 import reactor.core.publisher.Mono;
 
@@ -19,15 +17,20 @@ import reactor.core.publisher.Mono;
 public class BlobStorageClientAzure implements BlobStorageClient {
 
   private final BlobStorageConfig blobStorageConfig;
+  private final BlobServiceClientBuilder blobServiceClientBuilder;
 
-  public BlobStorageClientAzure(BlobStorageConfig blobStorageConfig){
+  public BlobStorageClientAzure(BlobStorageConfig blobStorageConfig,
+      BlobServiceClientBuilder blobServiceClientBuilder) {
     this.blobStorageConfig = blobStorageConfig;
+    this.blobServiceClientBuilder = blobServiceClientBuilder;
   }
 
   @Override
   public Mono<ServiceProviderDataResponse> getServiceProviderData() {
-    BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-        .endpoint(blobStorageConfig.storageAccountName())
+    String endpoint = String.format("https://%s.blob.core.windows.net", blobStorageConfig.storageAccountName());
+
+    BlobServiceClient blobServiceClient = blobServiceClientBuilder
+        .endpoint(endpoint)
         .credential(new DefaultAzureCredentialBuilder().build())
         .buildClient();
 
@@ -39,5 +42,4 @@ public class BlobStorageClientAzure implements BlobStorageClient {
 
     return Mono.just(blobClient.downloadContent().toObject(ServiceProviderDataResponse.class));
   }
-
 }
---
+++ src/test/java/it/gov/pagopa/rtp/activator/integration/blobstorage/BlobStorageClientAzureTest.java
@@ -0,0 +1,78 @@
+package it.gov.pagopa.rtp.activator.integration.blobstorage;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.azure.core.util.BinaryData;
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+
+import it.gov.pagopa.rtp.activator.configuration.BlobStorageConfig;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+class BlobStorageClientAzureTest {
+
+    @Mock
+    private BlobStorageConfig blobStorageConfig;
+
+    @Mock
+    private BlobServiceClient blobServiceClient;
+
+    @Mock
+    private BlobContainerClient blobContainerClient;
+
+    @Mock
+    private BlobServiceClientBuilder blobServiceClientBuilder;
+
+    @Mock
+    private BlobClient blobClient;
+
+    @InjectMocks
+    private BlobStorageClientAzure blobStorageClient;
+
+    @BeforeEach
+    void setup() {
+        blobStorageClient = new BlobStorageClientAzure(blobStorageConfig, blobServiceClientBuilder);
+    }
+
+    @Test
+    void testGetServiceProviderData() {
+        when(blobStorageConfig.storageAccountName()).thenReturn("mystorage");
+        when(blobStorageConfig.containerName()).thenReturn("mycontainer");
+        when(blobStorageConfig.blobName()).thenReturn("myblob.json");
+            
+
+        when(blobServiceClientBuilder.endpoint(any(String.class)))
+                .thenReturn(blobServiceClientBuilder);
+        when(blobServiceClientBuilder.credential(any(DefaultAzureCredential.class)))
+                .thenReturn(blobServiceClientBuilder);
+        when(blobServiceClientBuilder.buildClient())
+                .thenReturn(blobServiceClient);
+
+        when(blobServiceClient.getBlobContainerClient(anyString()))
+                .thenReturn(blobContainerClient);
+        when(blobContainerClient.getBlobClient(anyString()))
+                .thenReturn(blobClient);
+
+        ServiceProviderDataResponse mockResponse = new ServiceProviderDataResponse("test", "testuri");
+        when(blobClient.downloadContent())
+                .thenReturn(BinaryData.fromObject(mockResponse));
+
+        StepVerifier.create(blobStorageClient.getServiceProviderData())
+            .expectNext(mockResponse)
+            .verifyComplete();
+    }
+}
