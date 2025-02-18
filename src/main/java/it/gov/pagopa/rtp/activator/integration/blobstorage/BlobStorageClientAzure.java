04a98c19e7e42c30d32acf88d8643f9e7c21616d
Luca Consalvi <luca.consalvi@pagopa.it>
2025-02-18 09:56:26 +0100

    add some logs

--- src/main/java/it/gov/pagopa/rtp/activator/integration/blobstorage/BlobStorageClientAzure.java
+++ src/main/java/it/gov/pagopa/rtp/activator/integration/blobstorage/BlobStorageClientAzure.java
@@ -8,12 +8,14 @@ import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import it.gov.pagopa.rtp.activator.configuration.BlobStorageConfig;
 import reactor.core.publisher.Mono;
+import lombok.extern.slf4j.Slf4j;
 
 /*
  * Interact with the Azure Blob Storage using the Azure SDK Library, 
  * and performing 
  */
 @Component
+@Slf4j
 public class BlobStorageClientAzure implements BlobStorageClient {
 
   private final BlobStorageConfig blobStorageConfig;
@@ -27,19 +29,26 @@ public class BlobStorageClientAzure implements BlobStorageClient {
 
   @Override
   public Mono<ServiceProviderDataResponse> getServiceProviderData() {
+    log.info("Starting getServiceProviderData");
     String endpoint = String.format("https://%s.blob.core.windows.net", blobStorageConfig.storageAccountName());
 
     BlobServiceClient blobServiceClient = blobServiceClientBuilder
         .endpoint(endpoint)
         .credential(new DefaultAzureCredentialBuilder().build())
         .buildClient();
+    
+    log.info("Before container client");
 
     BlobContainerClient containerClient = blobServiceClient
         .getBlobContainerClient(blobStorageConfig.containerName());
 
+    log.info("Before blob client");
+
     BlobClient blobClient = containerClient
         .getBlobClient(blobStorageConfig.blobName());
 
+    log.info("Before download content");
     return Mono.just(blobClient.downloadContent().toObject(ServiceProviderDataResponse.class));
+
   }
 }
