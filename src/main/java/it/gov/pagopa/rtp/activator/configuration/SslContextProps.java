18845bed69d8aee0c0846aca638a6419638c7797
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-02-26 16:09:08 +0100

    feat: [SRTP-302] update client.ssl props with correct env vars names

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/SslContextProps.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/SslContextProps.java
@@ -2,8 +2,10 @@ package it.gov.pagopa.rtp.activator.configuration;
 
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.lang.NonNull;
 import org.springframework.validation.annotation.Validated;
+
 
 @Validated
 @ConfigurationProperties(prefix = "client.ssl")
@@ -12,7 +14,7 @@ public record SslContextProps(
     @NonNull
     String pfxFile,
 
-    @NotBlank
+    @DefaultValue("")
     String pfxPassword,
 
     @NotBlank
