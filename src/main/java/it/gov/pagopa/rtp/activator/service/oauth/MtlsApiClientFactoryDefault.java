a9d0e7b3e021c902a3b956c3e4f67659662ebf14
Luca Consalvi <luca.consalvi@pagopa.it>
2025-02-28 17:14:19 +0100

    fix some parts

--- src/main/java/it/gov/pagopa/rtp/activator/service/oauth/MtlsApiClientFactoryDefault.java
+++ src/main/java/it/gov/pagopa/rtp/activator/service/oauth/MtlsApiClientFactoryDefault.java
@@ -1,32 +1,20 @@
 package it.gov.pagopa.rtp.activator.service.oauth;
 
-
-import reactor.netty.http.client.HttpClient;
-
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import io.netty.handler.ssl.SslContext;
+import it.gov.pagopa.rtp.activator.configuration.MtlsWebClient.MtlsWebClientFactory;
 import it.gov.pagopa.rtp.activator.epcClient.invoker.ApiClient;
 
 public class MtlsApiClientFactoryDefault implements MtlsApiClientFactory {
 
-  private final SslContext sslContextFactory;
+  private final MtlsWebClientFactory mtlsWebClientFactory;
 
-  public MtlsApiClientFactoryDefault(SslContext sslContextFactory) {
-    this.sslContextFactory = sslContextFactory;
+  public MtlsApiClientFactoryDefault(MtlsWebClientFactory mtlsWebClientFactory) {
+    this.mtlsWebClientFactory = mtlsWebClientFactory;
   }
 
   @Override
   public ApiClient createMtlsApiClient(String basePath) {
-    SslContext sslContext = sslContextFactory;
 
-    HttpClient httpClient = HttpClient.create().secure(sslContextSpec -> sslContextSpec.sslContext(sslContext));
-
-    WebClient webClient = WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient))
-        .baseUrl(basePath).build();
-
-    ApiClient apiClient = new ApiClient(webClient);
+    ApiClient apiClient = new ApiClient(mtlsWebClientFactory.createMtlsWebClient());
 
     apiClient.setBasePath(basePath);
 
