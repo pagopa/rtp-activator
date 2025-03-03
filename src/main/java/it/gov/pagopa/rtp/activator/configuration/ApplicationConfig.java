5eaa32a345b92708ad1116fdafdcf979f24308d5
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-03-03 12:06:13 +0100

    feat: [SRTP-304] inject WebClient with SSL context to EPC client

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/ApplicationConfig.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/ApplicationConfig.java
@@ -2,34 +2,30 @@ package it.gov.pagopa.rtp.activator.configuration;
 
 import it.gov.pagopa.rtp.activator.activateClient.api.ReadApi;
 import it.gov.pagopa.rtp.activator.activateClient.invoker.ApiClient;
-import it.gov.pagopa.rtp.activator.epcClient.api.DefaultApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ApplicationConfig {
 
-  @Bean
+  @Bean("webClient")
+  @Primary
   public WebClient webClient() {
     return WebClientInterceptor.createWebClient(WebClient.builder());
   }
 
-  @Bean
+
+  @Bean("activationApiClient")
   public ApiClient apiClient(WebClient webClient) {
     return new ApiClient(webClient);
   }
 
-  @Bean
+
+  @Bean("activationApi")
   public ReadApi readApi(ApiClient apiClient) {
     return new ReadApi(apiClient);
-  }
-
-  @Bean
-  public DefaultApi defaultApi(ServiceProviderConfig serviceProviderConfig) {
-    var httpClient = new DefaultApi();
-    httpClient.getApiClient().setBasePath(serviceProviderConfig.send().epcMockUrl());
-    return httpClient;
   }
 
 }
