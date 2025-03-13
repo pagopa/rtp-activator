bce4a914f82f2cae7bbf9bf310fe3af2436f46fc
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-03-13 16:08:08 +0100

    feat: [SRTP-351] add Oauth2Handler

---
+++ src/main/java/it/gov/pagopa/rtp/activator/service/rtp/handler/Oauth2Handler.java
@@ -0,0 +1,61 @@
+package it.gov.pagopa.rtp.activator.service.rtp.handler;
+
+import it.gov.pagopa.rtp.activator.service.oauth.Oauth2TokenService;
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+
+@Component("oauth2Handler")
+@Slf4j
+public class Oauth2Handler implements RequestHandler<EpcRequest> {
+
+  private static final String CLIENT_SECRET_ENV_VAR_PATTERN = "client.%s";
+
+  private final Oauth2TokenService oauth2TokenService;
+  private final Environment environment;
+
+
+  public Oauth2Handler(
+      @NonNull final Oauth2TokenService oauth2TokenService,
+      @NonNull final Environment environment) {
+
+    this.oauth2TokenService = Objects.requireNonNull(oauth2TokenService);
+    this.environment = Objects.requireNonNull(environment);
+  }
+
+
+  @NonNull
+  @Override
+  public Mono<EpcRequest> handle(@NonNull final EpcRequest request) {
+    return Mono.just(request)
+        .doOnNext(req -> log.info("Handling OAuth2 for {}", req.serviceProviderFullData().spName()))
+        .filter(req -> req.serviceProviderFullData().tsp().oauth2() != null)
+        .doOnNext(req -> log.info("Retrieving access token"))
+        .flatMap(this::callOauth2TokenService)
+        .doOnNext(req -> log.info("Successfully retrieved access token"))
+        .switchIfEmpty(Mono.fromSupplier(() -> {
+          log.info("Skipping OAuth2 token retrieval");
+          return request;
+        }));
+  }
+
+
+  @NonNull
+  private Mono<EpcRequest> callOauth2TokenService(@NonNull final EpcRequest request) {
+    final var oauthData = request.serviceProviderFullData().tsp().oauth2();
+    final var clientSecret = this.environment.getProperty(
+        String.format(CLIENT_SECRET_ENV_VAR_PATTERN, oauthData.clientSecretEnvVar())
+    );
+
+    return this.oauth2TokenService.getAccessToken(
+            oauthData.tokenEndpoint(),
+            oauthData.clientId(),
+            clientSecret,
+            oauthData.scope())
+        .map(request::withToken);
+  }
+}
