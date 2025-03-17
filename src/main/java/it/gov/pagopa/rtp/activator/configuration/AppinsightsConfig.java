db8f4f850bcaa160ca2a959e8442d7023580f5e3
TommasoLencioni <tommaso.lencioni@pagopa.it>
2025-03-17 14:27:22 +0100

    feat: [SRTP-327] update Azure Monitor exporter configuration to use connection string

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/AppinsightsConfig.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/AppinsightsConfig.java
@@ -4,17 +4,21 @@ import com.azure.monitor.opentelemetry.autoconfigure.AzureMonitorAutoConfigure;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppinsightsConfig {
 
+  @Value("${spring.cloud.azure.monitor.connection-string}")
+  private String connectionString;
+
   @Bean
   public OpenTelemetry configureAzureMonitorExporter() {
     AutoConfiguredOpenTelemetrySdkBuilder sdkBuilder = AutoConfiguredOpenTelemetrySdk.builder();
 
-    AzureMonitorAutoConfigure.customize(sdkBuilder);
+    AzureMonitorAutoConfigure.customize(sdkBuilder, connectionString);
 
     return sdkBuilder.build().getOpenTelemetrySdk();
   }
