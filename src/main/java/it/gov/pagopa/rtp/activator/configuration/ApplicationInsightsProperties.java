484c24e475d4858b63e8b56115b3a910529c2bd9
Luca Consalvi <luca.consalvi@pagopa.it>
2025-03-18 17:25:25 +0100

    revert last mod

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/ApplicationInsightsProperties.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/ApplicationInsightsProperties.java
@@ -2,7 +2,7 @@ package it.gov.pagopa.rtp.activator.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "applicationinsights")
+@ConfigurationProperties(prefix = "spring.cloud.azure.monitor")
 public record ApplicationInsightsProperties(String connectionString) {
     
 }
