135f096456a11722d9355c13417d616fa8f04839
Luca Consalvi <luca.consalvi@pagopa.it>
2025-03-18 15:22:53 +0100

    add a configprop for the app inights connection string

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/AppinsightsConfig.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/AppinsightsConfig.java
@@ -4,25 +4,24 @@ import com.azure.monitor.opentelemetry.autoconfigure.AzureMonitorAutoConfigure;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppinsightsConfig {
 
-  private final String connectionString;
+  private final ApplicationInsightsProperties applicationInsightsProperties;
 
 
-  public AppinsightsConfig(@Value("${spring.cloud.azure.monitor.connection-string}") String connectionString) {
-    this.connectionString = connectionString;
+  public AppinsightsConfig(ApplicationInsightsProperties applicationInsightsProperties) {
+    this.applicationInsightsProperties = applicationInsightsProperties;
   }
 
   @Bean
   public OpenTelemetry configureAzureMonitorExporter() {
     AutoConfiguredOpenTelemetrySdkBuilder sdkBuilder = AutoConfiguredOpenTelemetrySdk.builder();
 
-    AzureMonitorAutoConfigure.customize(sdkBuilder, connectionString);
+    AzureMonitorAutoConfigure.customize(sdkBuilder, applicationInsightsProperties.connectionString());
 
     return sdkBuilder.build().getOpenTelemetrySdk();
   }
