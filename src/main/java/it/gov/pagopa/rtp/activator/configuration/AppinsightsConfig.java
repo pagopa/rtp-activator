13e062e0b0f33a56b38f938a0b6bfd606964d2b7
Luca Consalvi <luca.consalvi@pagopa.it>
2025-03-19 12:28:15 +0100

    [SRTP-327] add annotations

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/AppinsightsConfig.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/AppinsightsConfig.java
@@ -6,12 +6,16 @@ import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Role;
 
 @Slf4j
 @Configuration
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class AppinsightsConfig {
 
   private final ApplicationInsightsProperties applicationInsightsProperties;
@@ -22,6 +26,8 @@ public class AppinsightsConfig {
 
   @Bean
   @Primary
+  @Lazy
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
   public OpenTelemetry configureAzureMonitorExporter() {
     AutoConfiguredOpenTelemetrySdkBuilder sdkBuilder = AutoConfiguredOpenTelemetrySdk.builder();
     AzureMonitorAutoConfigure.customize(sdkBuilder, applicationInsightsProperties.connectionString());
