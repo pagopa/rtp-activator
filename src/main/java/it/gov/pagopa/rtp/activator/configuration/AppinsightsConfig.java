952d4a9f70c42b0335f66ec34fe7c67a6ccaa4ad
Luca Consalvi <luca.consalvi@pagopa.it>
2025-03-19 12:11:01 +0100

    [SRTP-327] test with primary annotation

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/AppinsightsConfig.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/AppinsightsConfig.java
@@ -5,8 +5,10 @@ import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Slf4j
 @Configuration
@@ -19,9 +21,9 @@ public class AppinsightsConfig {
   }
 
   @Bean
+  @Primary
   public OpenTelemetry configureAzureMonitorExporter() {
     AutoConfiguredOpenTelemetrySdkBuilder sdkBuilder = AutoConfiguredOpenTelemetrySdk.builder();
-    log.info(applicationInsightsProperties.connectionString().substring(0, 20));
     AzureMonitorAutoConfigure.customize(sdkBuilder, applicationInsightsProperties.connectionString());
 
     return sdkBuilder.build().getOpenTelemetrySdk();
