a02d2c1ba716caf6483d0dc2db3ef45a94fd4915
TommasoLencioni <tommaso.lencioni@pagopa.it>
2025-03-19 11:08:42 +0100

    test: [SRTP-327] print starting chars of connection string

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/AppinsightsConfig.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/AppinsightsConfig.java
@@ -4,9 +4,11 @@ import com.azure.monitor.opentelemetry.autoconfigure.AzureMonitorAutoConfigure;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class AppinsightsConfig {
 
@@ -19,7 +21,7 @@ public class AppinsightsConfig {
   @Bean
   public OpenTelemetry configureAzureMonitorExporter() {
     AutoConfiguredOpenTelemetrySdkBuilder sdkBuilder = AutoConfiguredOpenTelemetrySdk.builder();
-    
+    log.info(applicationInsightsProperties.connectionString().substring(0, 20));
     AzureMonitorAutoConfigure.customize(sdkBuilder, applicationInsightsProperties.connectionString());
 
     return sdkBuilder.build().getOpenTelemetrySdk();
