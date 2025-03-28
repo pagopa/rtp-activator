938dab8787ae8ec182bc10dfab541f8c4ee2fad2
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-03-28 11:38:50 +0100

    fix: [SRTP-339] remove unneeded @With annotation in PagoPaConfigProperties

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/PagoPaConfigProperties.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/PagoPaConfigProperties.java
@@ -2,19 +2,16 @@ package it.gov.pagopa.rtp.activator.configuration;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.With;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 
-@With
 @Validated
 @ConfigurationProperties(prefix = "pagopa")
 public record PagoPaConfigProperties(
     @NotNull Anag anag
 ) {
 
-    @With
     public record Anag(
         @NotBlank String iban,
         @NotBlank String fiscalCode) {}
