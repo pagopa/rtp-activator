615ae0788bcc905c0a9465545f869cd271401d81
Tommaso Lencioni <tommaso.lencioni@pagopa.it>
2025-04-02 10:43:36 +0200

    feat: [SRTP-350]  Add info on RTP send log (#87)

--- src/main/java/it/gov/pagopa/rtp/activator/controller/rtp/SendAPIControllerImpl.java
+++ src/main/java/it/gov/pagopa/rtp/activator/controller/rtp/SendAPIControllerImpl.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.rtp.activator.controller.rtp;
 
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import it.gov.pagopa.rtp.activator.configuration.ServiceProviderConfig;
 import it.gov.pagopa.rtp.activator.controller.generated.send.RtpsApi;
 import it.gov.pagopa.rtp.activator.domain.errors.PayerNotActivatedException;
@@ -9,6 +10,7 @@ import it.gov.pagopa.rtp.activator.service.rtp.SendRTPService;
 import it.gov.pagopa.rtp.activator.utils.TokenInfo;
 import java.net.URI;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
@@ -33,6 +35,7 @@ public class SendAPIControllerImpl implements RtpsApi {
     this.serviceProviderConfig = serviceProviderConfig;
   }
 
+  @WithSpan
   @Override
   @PreAuthorize("hasRole('write_rtp_send')")
   public Mono<ResponseEntity<Void>> createRtp(Mono<CreateRtpDto> createRtpDto,
@@ -42,6 +45,10 @@ public class SendAPIControllerImpl implements RtpsApi {
         .flatMap(rtpDto -> TokenInfo.getTokenSubject()
             .map(sub -> rtpDtoMapper.toRtpWithServiceProviderCreditor(rtpDto, sub)))
         .flatMap(sendRTPService::send)
+        .doOnSuccess(rtpSaved -> MDC.put("debtor_service_provider", rtpSaved.serviceProviderDebtor()))
+        .doOnSuccess(rtpSaved -> MDC.put("creditor_service_provider", rtpSaved.serviceProviderCreditor()))
+        .doOnSuccess(rtpSaved -> MDC.put("payee_name", rtpSaved.payeeName()))
+        .doOnSuccess(rtpSaved -> log.info("RTP sent with id: {}", rtpSaved.resourceID().getId()))
         .<ResponseEntity<Void>>map(rtp -> ResponseEntity
             .created(URI.create(serviceProviderConfig.baseUrl() + rtp.resourceID().getId()))
             .build())
@@ -49,7 +56,8 @@ public class SendAPIControllerImpl implements RtpsApi {
             ResponseEntity.unprocessableEntity().build())
         .onErrorReturn(ServiceProviderNotFoundException.class,
             ResponseEntity.unprocessableEntity().build())
-        .doOnError(a -> log.error("Error creating RTP {}", a.getMessage()));
+        .doOnError(a -> log.error("Error creating RTP {}", a.getMessage()))
+        .doFinally(f -> MDC.clear());
   }
 
 }
