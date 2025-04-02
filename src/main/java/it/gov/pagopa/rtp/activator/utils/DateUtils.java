514d2cd86a8f6cc08d1520ff4d600a6986481ad3
Davide Ragusi <davideragui>
2025-04-02 12:09:04 +0200

    replace: [SRTP-392] create DateUtils and replace the date creDtTm in zulu format

---
+++ src/main/java/it/gov/pagopa/rtp/activator/utils/DateUtils.java
@@ -0,0 +1,22 @@
+package it.gov.pagopa.rtp.activator.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.util.Optional;
+
+@Component
+public class DateUtils {
+
+    public static String savingLocalDateTimeToZulu(LocalDateTime localDateTime) {
+        return Optional.of(localDateTime)
+            .map(sdt-> sdt.toInstant(ZoneOffset.UTC))
+            .map(sdt -> sdt.with(ChronoField.NANO_OF_SECOND, 0))
+            .map(DateTimeFormatter.ISO_INSTANT::format)
+            .orElseThrow(() -> new IllegalArgumentException("Couldn't convert saving datetime to Zulu format"));
+    }
+
+}
