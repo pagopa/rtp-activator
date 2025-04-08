9f5a03516b769ee0139005e3bf9e082a3657911e
Davide Ragusi <davideragui>
2025-04-08 11:49:03 +0200

    fix: [SRTP-392] fix null handling and method rename

--- src/main/java/it/gov/pagopa/rtp/activator/utils/DateUtils.java
+++ src/main/java/it/gov/pagopa/rtp/activator/utils/DateUtils.java
@@ -8,11 +8,10 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.util.Optional;
 
-@Component
 public class DateUtils {
 
-    public static String LocalDateTimeToZulu(LocalDateTime localDateTime) {
-        return Optional.of(localDateTime)
+    public static String localDateTimeToZulu(LocalDateTime localDateTime) {
+        return Optional.ofNullable(localDateTime)
             .map(sdt-> sdt.toInstant(ZoneOffset.UTC))
             .map(sdt -> sdt.with(ChronoField.NANO_OF_SECOND, 0))
             .map(DateTimeFormatter.ISO_INSTANT::format)
