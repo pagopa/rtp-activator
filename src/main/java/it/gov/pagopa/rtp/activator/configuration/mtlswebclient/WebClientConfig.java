d7b4911cb0b3fa8ad8f2a901d08e3bdf7cb78c0b
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-04-17 16:33:47 +0200

    refactor: [SRTP-419] polish WebClient.builder bean creation

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/mtlswebclient/WebClientConfig.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/mtlswebclient/WebClientConfig.java
@@ -2,22 +2,25 @@ package it.gov.pagopa.rtp.activator.configuration.mtlswebclient;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.spring.webflux.v5_3.SpringWebfluxClientTelemetry;
+import java.util.Objects;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.lang.NonNull;
 import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Configuration
 public class WebClientConfig {
 
-    @Bean
-    @Primary
+    @Bean("webClientBuilder")
+    @NonNull
     public WebClient.Builder webClientBuilder(@NonNull final OpenTelemetry openTelemetry){
+        Objects.requireNonNull(openTelemetry, "OpenTelemetry bean cannot be null.");
+
         final var springWebfluxClientTelemetry = SpringWebfluxClientTelemetry.builder(openTelemetry)
             .build();
 
-        final var webclient = WebClient.create();
-        return webclient.mutate().filters(springWebfluxClientTelemetry::addFilter);
+        return WebClient.builder()
+            .filters(springWebfluxClientTelemetry::addFilter);
     }
 }
