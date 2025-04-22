848e35b3274f9900642d401daa6ae59e124829ad
Davide Ragusi <davideragui>
2025-04-22 16:34:49 +0200

    fix: [SRTP-395] remove unused import on repository RtpDB

--- src/main/java/it/gov/pagopa/rtp/activator/repository/rtp/RtpDB.java
+++ src/main/java/it/gov/pagopa/rtp/activator/repository/rtp/RtpDB.java
@@ -1,7 +1,5 @@
 package it.gov.pagopa.rtp.activator.repository.rtp;
 
-
-import java.util.UUID;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
