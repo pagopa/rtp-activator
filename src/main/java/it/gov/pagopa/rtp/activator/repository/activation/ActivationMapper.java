1cfce644b635cb8585cb7ca103baa06ecb81132d
Davide Ragusi <davideragui>
2025-04-23 11:22:36 +0200

    refactor: [SRTP-395] refactor: convert ActivationID from string to binary UUID

--- src/main/java/it/gov/pagopa/rtp/activator/repository/activation/ActivationMapper.java
+++ src/main/java/it/gov/pagopa/rtp/activator/repository/activation/ActivationMapper.java
@@ -1,7 +1,5 @@
 package it.gov.pagopa.rtp.activator.repository.activation;
 
-import java.util.UUID;
-
 import org.springframework.stereotype.Component;
 
 import it.gov.pagopa.rtp.activator.domain.payer.Payer;
@@ -11,15 +9,14 @@ import it.gov.pagopa.rtp.activator.domain.payer.ActivationID;
 public class ActivationMapper {
 
     public Payer toDomain(ActivationEntity activationEntity) {
-        ActivationID activationID = new ActivationID(
-                UUID.fromString(activationEntity.getId()));
+        ActivationID activationID = new ActivationID((activationEntity.getId()));
         return new Payer(activationID,
                 activationEntity.getServiceProviderDebtor(), activationEntity.getFiscalCode(),
                 activationEntity.getEffectiveActivationDate());
     }
 
     public ActivationEntity toDbEntity(Payer payer) {
-        return ActivationEntity.builder().id(payer.activationID().getId().toString())
+        return ActivationEntity.builder().id(payer.activationID().getId())
                 .fiscalCode(payer.fiscalCode())
                 .serviceProviderDebtor(payer.serviceProviderDebtor())
                 .effectiveActivationDate(payer.effectiveActivationDate())
