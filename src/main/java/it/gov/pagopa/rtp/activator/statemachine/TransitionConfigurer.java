9cb9faf2fbc7ba5acf51e505c94707ea231144d1
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-04-28 11:07:47 +0200

    chore: [SRTP-426] correctly use generic type in TransitionConfigurer

--- src/main/java/it/gov/pagopa/rtp/activator/statemachine/TransitionConfigurer.java
+++ src/main/java/it/gov/pagopa/rtp/activator/statemachine/TransitionConfigurer.java
@@ -1,18 +1,17 @@
 package it.gov.pagopa.rtp.activator.statemachine;
 
-import it.gov.pagopa.rtp.activator.repository.rtp.RtpEntity;
 import java.util.List;
 import java.util.function.Consumer;
 
 public interface TransitionConfigurer<T, S, E> {
 
   TransitionConfigurer<T, S, E> register(TransitionKey<S, E> transitionKey, S toState);
-  TransitionConfigurer<T, S, E> register(TransitionKey<S, E> transitionKey, S toState, Consumer<RtpEntity> action);
+  TransitionConfigurer<T, S, E> register(TransitionKey<S, E> transitionKey, S toState, Consumer<T> action);
   TransitionConfigurer<T, S, E> register(
       TransitionKey<S, E> transitionKey,
       S toState,
-      List<Consumer<RtpEntity>> preTransitionAction,
-      List<Consumer<RtpEntity>> postTransitionAction);
+      List<Consumer<T>> preTransitionAction,
+      List<Consumer<T>> postTransitionAction);
 
   TransitionConfiguration<T, S, E> build();
 
