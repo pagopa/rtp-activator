b4bbe6ae17161b733600451d8d2c837bb771887c
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-04-28 16:09:01 +0200

    feat: [SRTP-426] validate pre and post transaction actions in RtpTransitionConfigurer
    # Conflicts:
    #	src/main/java/it/gov/pagopa/rtp/activator/statemachine/RtpTransitionConfigurer.java

--- src/main/java/it/gov/pagopa/rtp/activator/statemachine/RtpTransitionConfigurer.java
+++ src/main/java/it/gov/pagopa/rtp/activator/statemachine/RtpTransitionConfigurer.java
@@ -6,8 +6,12 @@ import it.gov.pagopa.rtp.activator.repository.rtp.RtpEntity;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 public class RtpTransitionConfigurer implements TransitionConfigurer<RtpEntity, RtpStatus, RtpEvent> {
 
@@ -18,7 +22,8 @@ public class RtpTransitionConfigurer implements TransitionConfigurer<RtpEntity, 
 
   @Override
   public TransitionConfigurer<RtpEntity, RtpStatus, RtpEvent> register(
-      TransitionKey<RtpStatus, RtpEvent> transitionKey, RtpStatus toState) {
+      @NonNull final TransitionKey<RtpStatus, RtpEvent> transitionKey,
+      @NonNull final RtpStatus toState) {
 
     return this.register(transitionKey, toState, NOOP_ACTION);
   }
@@ -26,20 +31,37 @@ public class RtpTransitionConfigurer implements TransitionConfigurer<RtpEntity, 
 
   @Override
   public TransitionConfigurer<RtpEntity, RtpStatus, RtpEvent> register(
-      TransitionKey<RtpStatus, RtpEvent> transitionKey, RtpStatus toState,
-      Consumer<RtpEntity> action) {
+      @NonNull final TransitionKey<RtpStatus, RtpEvent> transitionKey,
+      @NonNull final RtpStatus toState,
+      @Nullable final Consumer<RtpEntity> action) {
 
-    return this.register(transitionKey, toState, Collections.emptyList(), Collections.singletonList(action));
+    final var validatedAction = Optional.ofNullable(action)
+        .orElse(NOOP_ACTION);
+
+    return this.register(
+        transitionKey, toState,
+        Collections.emptyList(), Collections.singletonList(validatedAction));
   }
 
 
   @Override
   public TransitionConfigurer<RtpEntity, RtpStatus, RtpEvent> register(
-      TransitionKey<RtpStatus, RtpEvent> transitionKey, RtpStatus toState,
-      List<Consumer<RtpEntity>> preTransitionAction, List<Consumer<RtpEntity>> postTransitionAction) {
+      @NonNull final TransitionKey<RtpStatus, RtpEvent> transitionKey,
+      @NonNull final RtpStatus toState,
+      @Nullable final List<Consumer<RtpEntity>> preTransitionActions,
+      @Nullable final List<Consumer<RtpEntity>> postTransitionActions) {
+
+    Objects.requireNonNull(transitionKey, "transitionKey cannot be null.");
+    Objects.requireNonNull(toState, "Destination state cannot be null.");
+
+    final var validatedPreTransactionActions = Optional.ofNullable(preTransitionActions)
+        .orElseGet(Collections::emptyList);
+
+    final var validatedPostTransactionActions = Optional.ofNullable(postTransitionActions)
+        .orElseGet(Collections::emptyList);
 
     final var transition = new RtpTransition(
-        transitionKey.getSource(), transitionKey.getEvent(), toState, preTransitionAction, postTransitionAction);
+        transitionKey.getSource(), transitionKey.getEvent(), toState, validatedPreTransactionActions, validatedPostTransactionActions);
 
     this.transitionsMap.put((RtpTransitionKey) transitionKey, transition);
 
