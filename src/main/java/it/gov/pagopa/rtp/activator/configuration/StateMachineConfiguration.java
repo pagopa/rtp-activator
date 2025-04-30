d6700fe6b2d1b886d9f42eeddce20915a1edbc3a
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-04-30 10:10:49 +0200

    fix: [SRTP-469] add missing transitions from CREATED state

--- src/main/java/it/gov/pagopa/rtp/activator/configuration/StateMachineConfiguration.java
+++ src/main/java/it/gov/pagopa/rtp/activator/configuration/StateMachineConfiguration.java
@@ -30,6 +30,12 @@ public class StateMachineConfiguration {
     return new RtpTransitionConfigurer()
         .register(new RtpTransitionKey(RtpStatus.CREATED, RtpEvent.SEND_RTP), RtpStatus.SENT, persistRtp())
         .register(new RtpTransitionKey(RtpStatus.CREATED, RtpEvent.ERROR_SEND_RTP), RtpStatus.ERROR_SEND, persistRtp())
+        .register(new RtpTransitionKey(RtpStatus.CREATED, RtpEvent.ACCEPT_RTP), RtpStatus.ACCEPTED, persistRtp())
+        .register(new RtpTransitionKey(RtpStatus.CREATED, RtpEvent.REJECT_RTP), RtpStatus.REJECTED, persistRtp())
+        .register(new RtpTransitionKey(RtpStatus.CREATED, RtpEvent.USER_ACCEPT_RTP), RtpStatus.USER_ACCEPTED, persistRtp())
+        .register(new RtpTransitionKey(RtpStatus.CREATED, RtpEvent.USER_REJECT_RTP), RtpStatus.USER_REJECTED, persistRtp())
+        .register(new RtpTransitionKey(RtpStatus.CREATED, RtpEvent.PAY_RTP), RtpStatus.PAYED, persistRtp())
+        .register(new RtpTransitionKey(RtpStatus.CREATED, RtpEvent.CANCEL_RTP), RtpStatus.CANCELLED, persistRtp())
 
         .register(new RtpTransitionKey(RtpStatus.SENT, RtpEvent.ACCEPT_RTP), RtpStatus.ACCEPTED, persistRtp())
         .register(new RtpTransitionKey(RtpStatus.SENT, RtpEvent.REJECT_RTP), RtpStatus.REJECTED, persistRtp())
