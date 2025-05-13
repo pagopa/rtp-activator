3ff7ec577c5095c1b00484c5e8b9b0eccaefedd2
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-05-13 11:26:01 +0200

    test: [SRTP-421] add SendRtpResponseHandlerTest

---
+++ src/test/java/it/gov/pagopa/rtp/activator/service/rtp/handler/SendRtpResponseHandlerTest.java
@@ -0,0 +1,127 @@
+package it.gov.pagopa.rtp.activator.service.rtp.handler;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import it.gov.pagopa.rtp.activator.domain.rtp.Rtp;
+import it.gov.pagopa.rtp.activator.domain.rtp.TransactionStatus;
+import it.gov.pagopa.rtp.activator.service.rtp.RtpStatusUpdater;
+import java.lang.reflect.InvocationTargetException;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+class SendRtpResponseHandlerTest {
+
+  @Mock
+  private RtpStatusUpdater rtpStatusUpdater;
+
+  @InjectMocks
+  private SendRtpResponseHandler sendRtpResponseHandler;
+
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+
+  @ParameterizedTest
+  @MethodSource("provideTransactionStatusAndExpectedMethodForSuccess")
+  public void givenValidRequest_whenTransactionStatusIsX_thenTriggerY(
+      TransactionStatus transactionStatus,
+      String expectedMethod)
+      throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+    final var rtpToSend = mock(Rtp.class);
+    final var request = new EpcRequest(rtpToSend, null, null, transactionStatus);
+
+    when(
+        rtpStatusUpdater.getClass()
+        .getMethod(expectedMethod, Rtp.class)
+        .invoke(rtpStatusUpdater, rtpToSend))
+        .thenReturn(Mono.just(rtpToSend));
+
+    final var result = sendRtpResponseHandler.handle(request);
+
+    StepVerifier.create(result)
+        .expectNextMatches(req -> req.rtpToSend().equals(rtpToSend))
+        .verifyComplete();
+  }
+
+  private static Stream<Arguments> provideTransactionStatusAndExpectedMethodForSuccess() {
+    return Stream.of(
+        Arguments.of(TransactionStatus.ACTC, "triggerAcceptRtp"),
+        Arguments.of(TransactionStatus.RJCT, "triggerRejectRtp"),
+        Arguments.of(TransactionStatus.ERROR, "triggerErrorSendRtp"),
+        Arguments.of(null, "triggerSendRtp")
+    );
+  }
+
+
+  @Test
+  public void givenValidRequest_whenTransactionStatusIsAccp_thenThrowIllegalStateException() {
+    final var request = mock(EpcRequest.class);
+    Rtp rtpToSend = mock(Rtp.class);
+    TransactionStatus transactionStatus = TransactionStatus.ACCP;
+
+    when(request.rtpToSend()).thenReturn(rtpToSend);
+    when(request.response()).thenReturn(transactionStatus);
+
+    // Act
+    Mono<EpcRequest> result = sendRtpResponseHandler.handle(request);
+
+    // Assert
+    StepVerifier.create(result)
+        .expectErrorMatches(throwable -> throwable instanceof IllegalStateException)
+        .verify();
+
+    verify(rtpStatusUpdater, never()).triggerSendRtp(any(Rtp.class));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideTransactionStatusForException")
+  public void givenValidRequest_whenTransactionStatusIsX_thenThrowException(
+      TransactionStatus transactionStatus,
+      Class<? extends Throwable> expectedException) {
+
+    final var rtpToSend = mock(Rtp.class);
+    EpcRequest request = mock(EpcRequest.class);
+
+    when(request.rtpToSend()).thenReturn(rtpToSend);
+    when(request.response()).thenReturn(transactionStatus);
+
+    final var result = sendRtpResponseHandler.handle(request);
+
+    StepVerifier.create(result)
+        .expectErrorMatches(throwable -> throwable.getClass().equals(expectedException))
+        .verify();
+
+    verifyNoInteractions(rtpStatusUpdater);
+  }
+
+  private static Stream<Arguments> provideTransactionStatusForException() {
+    return Stream.of(
+        Arguments.of(TransactionStatus.ACCP, IllegalStateException.class)
+    );
+  }
+
+  @Test
+  public void givenNullRequest_whenHandleCalled_thenThrowNullPointerException() {
+    assertThrows(NullPointerException.class, () -> sendRtpResponseHandler.handle(null));
+  }
+
+}
