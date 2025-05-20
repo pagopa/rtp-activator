1b41f03f81ef7ebd60da40d147e7a1c3822a4f44
Francesco Muscianisi <francesco.muscianisi@digitouch.it>
2025-05-20 15:08:02 +0200

    feat: [SRTP-506] add findById in PayerRepository

--- src/main/java/it/gov/pagopa/rtp/activator/domain/payer/PayerRepository.java
+++ src/main/java/it/gov/pagopa/rtp/activator/domain/payer/PayerRepository.java
@@ -1,10 +1,13 @@
 package it.gov.pagopa.rtp.activator.domain.payer;
 
 
+import java.util.UUID;
 import reactor.core.publisher.Mono;
 
 public interface PayerRepository {
-    
+
+    Mono<Payer> findById(UUID id);
+
     // Used to check if a specific payer is already registered.
     Mono<Payer> findByFiscalCode(String fiscalCode);
     
--- src/main/java/it/gov/pagopa/rtp/activator/repository/activation/ActivationDBRepository.java
+++ src/main/java/it/gov/pagopa/rtp/activator/repository/activation/ActivationDBRepository.java
@@ -1,6 +1,9 @@
 package it.gov.pagopa.rtp.activator.repository.activation;
 
 
+import java.util.Objects;
+import java.util.UUID;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import it.gov.pagopa.rtp.activator.domain.payer.Payer;
@@ -18,6 +21,17 @@ public class ActivationDBRepository implements PayerRepository {
         this.activationDB = activationDB;
         this.activationMapper = activationMapper;
     }
+
+
+    @NonNull
+    @Override
+    public Mono<Payer> findById(@NonNull final UUID id) {
+      Objects.requireNonNull(id, "Id cannot be null");
+
+      return this.activationDB.findById(id)
+          .map(activationMapper::toDomain);
+    }
+
 
     @Override
     public Mono<Payer> findByFiscalCode(String fiscalCode) {
