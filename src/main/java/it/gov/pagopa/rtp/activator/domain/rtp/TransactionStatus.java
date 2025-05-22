3b68c44b3d950abeda3ae0b8993477c52f3eb4bd
Davide Ragusi <davideragui>
2025-05-22 16:35:18 +0200

    feat: [SRTP-422] added fromString method for TransactionStatus

--- src/main/java/it/gov/pagopa/rtp/activator/domain/rtp/TransactionStatus.java
+++ src/main/java/it/gov/pagopa/rtp/activator/domain/rtp/TransactionStatus.java
@@ -1,7 +1,28 @@
 package it.gov.pagopa.rtp.activator.domain.rtp;
 
+import lombok.NonNull;
+import java.util.Arrays;
+
 public enum TransactionStatus {
 
-  ACTC, ACCP, RJCT, ERROR, CNCL, RJCR
+  ACTC("ACTC"),
+  ACCP("ACCP"),
+  RJCT("RJCT"),
+  ERROR("ERROR"),
+  CNCL("CNCL"),
+  RJCR("RJCR"),
+  ACWC("ACWC");
 
+  private final String value;
+
+  TransactionStatus(String value) {
+    this.value = value;
+  }
+
+  public static TransactionStatus fromString(@NonNull final String text) {
+    return Arrays.stream(TransactionStatus.values())
+            .filter(b -> b.value.equals(text))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("No matching Enum"));
+  }
 }
