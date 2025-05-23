b274eb56b56951acd406e0c43bae4c9ef0c3731e
Davide Ragusi <davideragui>
2025-05-23 16:35:39 +0200

    fix: [SRTP-422] fix JsonNodeUtils

--- src/main/java/it/gov/pagopa/rtp/activator/utils/JsonNodeUtils.java
+++ src/main/java/it/gov/pagopa/rtp/activator/utils/JsonNodeUtils.java
@@ -4,8 +4,10 @@ package it.gov.pagopa.rtp.activator.utils;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import org.springframework.lang.NonNull;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -14,12 +16,14 @@ import java.util.stream.StreamSupport;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JsonNodeUtils {
 
-    public static Stream<JsonNode> toStream(@NonNull JsonNode node) {
+    @NonNull
+    public static Collection<JsonNode> nodeToCollection(@NonNull final JsonNode node) {
         return Optional.of(node)
                 .filter(JsonNode::isContainerNode)
                 .map(n -> n.isArray()
                         ? StreamSupport.stream(n.spliterator(), false)
                         : Stream.of(n))
-                .orElseGet(Stream::empty);
+                .map(Stream::toList)
+                .orElseGet(List::of);
     }
 }
