827aeff25dd6b2fb2bf3076185ee9a9c26ec02be
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2025-06-26 12:36:00 +0200

    SRTP-551-fix-deleted-activations-fields

--- src/main/java/it/gov/pagopa/rtp/activator/repository/activation/DeletedActivationEntity.java
+++ src/main/java/it/gov/pagopa/rtp/activator/repository/activation/DeletedActivationEntity.java
@@ -1,15 +1,14 @@
 package it.gov.pagopa.rtp.activator.repository.activation;
 
-import java.time.Instant;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.time.Instant;
+import java.util.UUID;
 
 
 @Data
@@ -21,14 +20,7 @@ public class DeletedActivationEntity {
 
   @Id
   private UUID id;
-
-  @Field(name = "service_provider_debtor", targetType = FieldType.STRING)
   private String serviceProviderDebtor;
-
-  @Field(name = "deactivation_date", targetType = FieldType.DATE_TIME)
   private Instant deactivationDate;
-
-  @Field(name = "fiscal_code", targetType = FieldType.STRING)
   private String fiscalCode;
-
 }
