414cdb913a1a36d8989398f3f354133a36698264
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2025-07-03 14:57:22 +0200

    SRTP-653-update-deactivation-logs

--- src/main/java/it/gov/pagopa/rtp/activator/controller/activation/ActivationAPIControllerImpl.java
+++ src/main/java/it/gov/pagopa/rtp/activator/controller/activation/ActivationAPIControllerImpl.java
@@ -174,7 +174,6 @@ public class ActivationAPIControllerImpl implements CreateApi, ReadApi, DeleteAp
             payerToVerify -> Optional.of(payerToVerify)
                 .map(Payer::serviceProviderDebtor)
                 .orElse(""))
-                .doOnError(ex -> log.error("Error deactivating payer {}", ex.getMessage(), ex))
         )
 
 
@@ -184,13 +183,10 @@ public class ActivationAPIControllerImpl implements CreateApi, ReadApi, DeleteAp
         .doOnNext(deactivatedPayer -> log.info("Payer deactivated"))
         .map(deactivatedPayer -> ResponseEntity.noContent().<Void>build())
 
-//      .doOnError(ex -> log.error("Error deactivating payer {}", ex.getMessage(), ex))
-      .onErrorReturn(AccessDeniedException.class,
-             ResponseEntity.notFound().build())
-//        .onErrorResume(AccessDeniedException.class, ex -> {
-//          log.error("Error deactivating payer {}", ex.getMessage(), ex);
-//            return Mono.just(ResponseEntity.notFound().build());
-//          })
+        .onErrorResume(AccessDeniedException.class, ex -> {
+          log.error("Error deactivating payer {}", ex.getMessage(), ex);
+            return Mono.just(ResponseEntity.notFound().build());
+          })
 
             .switchIfEmpty(Mono.fromSupplier(() -> {
           log.error("Payer not found");
