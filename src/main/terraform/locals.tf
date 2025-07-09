cd6d98c66ce89b325a494fc25232cc4c1074fd92
Umberto Coppola Bottazzi <umberto.coppolabottazzi@pagopa.it>
2025-07-09 09:40:49 +0000

    feat: [SRTP-689] deploy@dev@itn (#147)
    Co-authored-by: Luca Consalvi <luca.consalvi@pagopa.it>

--- src/main/terraform/locals.tf
+++ src/main/terraform/locals.tf
@@ -5,6 +5,6 @@ locals {
   project = var.domain == "" ? "${var.prefix}-${var.env_short}" : "${var.prefix}-${var.env_short}-${var.domain}"
 
   rtp_kv_name = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-kv"
-  rtp_kv_resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-sec-rg"
+  rtp_kv_resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-${var.location_short == "itn" ? "security" : "sec"}-rg"
 }
 
