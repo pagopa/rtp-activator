94211bbe61b73e5f47c2ea63c7868ed06e67a46f
Luca Consalvi <luca.consalvi@pagopa.it>
2025-08-19 12:08:14 +0200

    [SRTP-808]: Formatting.

--- src/main/java/it/gov/pagopa/rtp/activator/controller/activation/ActivationDtoMapper.java
+++ src/main/java/it/gov/pagopa/rtp/activator/controller/activation/ActivationDtoMapper.java
@@ -13,25 +13,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class ActivationDtoMapper {
 
-    public ActivationDto toActivationDto(Payer payer) {
-        return new ActivationDto().id(payer.activationID().getId())
-                .payer(new PayerDto().fiscalCode(payer.fiscalCode()).rtpSpId(payer.serviceProviderDebtor()))
-                .effectiveActivationDate(LocalDateTime.ofInstant(payer.effectiveActivationDate(), ZoneOffset.UTC));
-    }
+  public ActivationDto toActivationDto(Payer payer) {
+    return new ActivationDto().id(payer.activationID().getId())
+        .payer(new PayerDto().fiscalCode(payer.fiscalCode()).rtpSpId(payer.serviceProviderDebtor()))
+        .effectiveActivationDate(LocalDateTime.ofInstant(payer.effectiveActivationDate(), ZoneOffset.UTC));
+  }
 
-    public PageOfActivationsDto toPageDto(List<Payer> activationEntityList, Long totalElements, int page, int size){
-        List<ActivationDto> activations = activationEntityList.stream()
-            .map(this::toActivationDto)
-            .toList();
+  public PageOfActivationsDto toPageDto(List<Payer> activationEntityList, Long totalElements, int page, int size) {
+    List<ActivationDto> activations = activationEntityList.stream()
+        .map(this::toActivationDto)
+        .toList();
 
-        PageMetadataDto metadata = new PageMetadataDto();
-        metadata.totalElements(totalElements);
-        metadata.totalPages((long) Math.ceil((double) totalElements / size));
-        metadata.page(page);
-        metadata.size(size);
+    PageMetadataDto metadata = new PageMetadataDto();
+    metadata.totalElements(totalElements);
+    metadata.totalPages((long) Math.ceil((double) totalElements / size));
+    metadata.page(page);
+    metadata.size(size);
 
-        return new PageOfActivationsDto()
-            .activations(activations)
-            .page(metadata);
-    }
+    return new PageOfActivationsDto()
+        .activations(activations)
+        .page(metadata);
+  }
 }
